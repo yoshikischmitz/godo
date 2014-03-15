95cf63e1169d363a6c180617660a42b2646ff94c
Yoshiki Schmitz <guiltyspark75@gmail.com>
2014-03-15 16:32:36 -0600

    add task completion functionality, add new task printer

--- godo.go
+++ godo.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-  "log"
-  "encoding/json"
+	"encoding/json"
 	"fmt"
 	"github.com/codegangsta/cli"
 	"io/ioutil"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -21,10 +22,10 @@ type Task struct {
 
 // Takes a json string and converts it to a Task struct,(without an index)
 func ParseTask(j string) Task {
-  var task Task
-  b := []byte(j)
-  json.Unmarshal(b, &task)
-  return task
+	var task Task
+	b := []byte(j)
+	json.Unmarshal(b, &task)
+	return task
 }
 
 // Returns an array of Tasks, with indices
@@ -32,14 +33,14 @@ func TaskList() []Task {
 	file, _ := ioutil.ReadFile("tasks.json")
 	file_str := string(file)
 	task_str_slice := strings.Split(file_str, "\n")
-  task_list := make([]Task, len(task_str_slice))
-  for i := range task_str_slice {
-      if task_str_slice[i] != "\n" {
-        if task_str_slice[i] != "" {
-          task_list[i] = ParseTask(task_str_slice[i])
-          task_list[i].Index = i
-        }
-      }
+	task_list := make([]Task, len(task_str_slice)-1)
+	for i := range task_str_slice {
+		if task_str_slice[i] != "\n" {
+			if task_str_slice[i] != "" {
+				task_list[i] = ParseTask(task_str_slice[i])
+				task_list[i].Index = i
+			}
+		}
 	}
 
 	return task_list
@@ -49,25 +50,25 @@ func TaskList() []Task {
 // String
 // Example:
 // task := Task {Priority: 0, Content: "get groceries, Date: time.Now(), Done: false}
-// task.String //= "0       newtask 2014-03-14 22:22:47.875460951 -0600 MDT false 
+// task.String //= "0       newtask 2014-03-14 22:22:47.875460951 -0600 MDT false
 func (t Task) String() string {
 	return fmt.Sprintf("%d\t%s\t%s\t%t", t.Priority, t.Content, t.Date, t.Done)
 }
 
 // Print a Task
 func (t *Task) Print() {
-	println(t.String())
+	fmt.Printf("[%d]\t[%s]\t[%s]\n", t.Index, t.Content, t.Date)
 }
 
 // Build a Task with Task.Content from string, with default values
 func buildTask(s string) Task {
-  tasks := TaskList()
+	tasks := TaskList()
 	task := Task{
 		Priority: 0,
 		Content:  s,
 		Date:     time.Now(),
 		Done:     false,
-    Index:    len(tasks)-1,
+		Index:    len(tasks) - 1,
 	}
 	return task
 }
@@ -75,19 +76,19 @@ func buildTask(s string) Task {
 // Append a new task to the end of the tasks file
 func WriteTask(task Task) error {
 	f, err := os.OpenFile("tasks.json", os.O_APPEND|os.O_WRONLY, 0600)
-  // check the error to see if we need to create a new file
+	// check the error to see if we need to create a new file
 	if err != nil {
-    f2, nErr := os.Create("tasks.json")
-    // if creation of new file fails, log it
-    if nErr != nil {
-      log.Fatal(nErr)
-    }
-    // assign the new file variable to the nil f
-    f = f2
+		f2, nErr := os.Create("tasks.json")
+		// if creation of new file fails, log it
+		if nErr != nil {
+			log.Fatal(nErr)
+		}
+		// assign the new file variable to the nil f
+		f = f2
 	}
-  json, _ := json.Marshal(task)
+	json, _ := json.Marshal(task)
 	f.Write(json)
-  f.WriteString("\n")
+	f.WriteString("\n")
 	return nil
 }
 
@@ -95,11 +96,22 @@ func WriteTask(task Task) error {
 func PrintTasks() {
 	task_list := TaskList()
 	for i := range task_list {
-    t := task_list[i]
-    if t.Done == false {
-      t.Print()
-    }
+		t := task_list[i]
+		if t.Done == false {
+			t.Print()
+		}
 	}
+}
+
+// Marks a task as complete by setting the complete field to true in the JSON file
+func CompleteTask(index int) {
+  task_list := TaskList()
+  task_list[index].Done = true
+  os.Remove("tasks.json")
+  for i := range task_list {
+    WriteTask(task_list[i])
+  }
+  fmt.Printf("Task Marked as complete: %s\n", task_list[index].Content)
 }
 
 func main() {
@@ -112,7 +124,7 @@ func main() {
 			Usage: "add a task",
 			Action: func(c *cli.Context) {
 				task := buildTask(c.Args().First())
-        WriteTask(task)
+				WriteTask(task)
 				fmt.Printf("Task is added: %s\n", task.Content)
 			},
 		},
@@ -121,6 +133,14 @@ func main() {
 			Usage: "lists all tasks",
 			Action: func(c *cli.Context) {
 				PrintTasks()
+			},
+		},
+		{
+			Name:  "complete",
+			Usage: "Marks a task specified by the integer argument as complete",
+			Action: func(c *cli.Context) {
+				index, _ := strconv.ParseInt(c.Args().First(), 10, 0)
+        CompleteTask(int(index))
 			},
 		},
 	}
